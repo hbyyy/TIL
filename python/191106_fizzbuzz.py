1fb507ba4b129d08cf11b40b9a517bf6ccf8d932
hbyyy <qjaduddl94@gmail.com>
2019-11-06 15:43:37 +0900

    thrid

--- python/191106_fizzbuzz.py
+++ python/191106_fizzbuzz.py
@@ -1,10 +1,9 @@
 
-for i in range(1, 100 +1):
-    if i % 3 == 0 or i % 5 == 0:
-        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
-    else:
-        print(i)
+fizzbuzz_list = [
+        'fizz' 
+        if i % 3 == 0 else 
+        'buzz' 
+        if i % 5 == 0 else
+        i for i in range(1, 100 +1)]
     
-
-        
-    
+print(fizzbuzz_list)
