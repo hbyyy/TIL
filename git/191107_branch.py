ae48ebecacbe96f0d276f0c79869a20b27ac90ea
hbyyy <qjaduddl94@gmail.com>
2019-11-07 12:07:07 +0900

    feat: implement fizzbuzz on list comprehention
    list comprehention fizzbuzz

--- git/191107_branch.py
+++ git/191107_branch.py
@@ -1,1 +1,9 @@
-print('fizzbuzz starts')
+fizzbuzz_list = ['fizzbzz'
+                if i % 15 == 0 else
+                'fizz'
+                if i % 3 == 0 else
+                'buzz'
+                if i % 5 == 0 else
+                i for i in range(1, 100 +1)]
+
+print(fizzbuzz_list)
